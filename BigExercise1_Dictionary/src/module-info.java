3d642e69e779b362bfd42c2060031adc4e8c3d4c
dungn8979 <dungn8979@gmail.com>
2021-10-12 21:13:24 +0700

    clean branch

--- BigExercise1_Dictionary/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module BigExercise1_Dictionary_2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires freetts;
-    requires java.sql;
-
-    opens Solution to javafx.fxml;
-    exports Solution;
-}
